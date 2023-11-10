5e03cccf27cd867c656930d610a1d5a24c8af265
i@alexsenkin.ru <@alexsenkin>
2023-11-10 15:04:00 +0300

    inti

---
+++ src/main/java/ru/senkin/connectionstate/ConnectionStateApplication.java
@@ -0,0 +1,13 @@
+package ru.senkin.connectionstate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConnectionStateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConnectionStateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/senkin/connectionstate/ConnectionStateApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.senkin.connectionstate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConnectionStateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
